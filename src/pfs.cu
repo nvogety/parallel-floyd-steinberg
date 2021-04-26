59780d74760a12d748137293d07f17e51348127b
Neeharika Vogety <neeharules@gmail.com>
2021-04-26 04:05:19 -0400

    get parallel version working, but slow

--- src/pfs.cu
+++ src/pfs.cu
@@ -3,17 +3,18 @@
 #include <algorithm>
 #define _USE_MATH_DEFINES
 #include <math.h>
+#include "CycleTimer.h"
 
 
 
-#define BLOCKDIMCOMP 32
-#define BLOCKSIZE 1024 // BLOCKDIMCOMP^2
+#define BLOCKDIMCOMP 16
+#define BLOCKSIZE 4 // BLOCKDIMCOMP^2
 #define FACTOR 4
 
-#define TOP_RIGHT_ERR_DIFF (7 / 16)
-#define BOT_LEFT_ERR_DIFF (3 / 16)
-#define BOT_MID_ERR_DIFF (5 / 16)
-#define BOT_RIGHT_ERR_DIFF (1 / 16)
+#define TOP_RIGHT_ERR_DIFF (7.0 / 16.0)
+#define BOT_LEFT_ERR_DIFF (3.0 / 16.0)
+#define BOT_MID_ERR_DIFF (5.0 / 16.0)
+#define BOT_RIGHT_ERR_DIFF (1.0 / 16.0)
 
 
 // #define SCAN_BLOCK_DIM 1024 // BLOCKSIZE 
@@ -21,14 +22,13 @@
 // #define NUMOFCIRCLESPERTHREAD 100 // NUMOFCIRCLESPERBLOCK/BLOCKSIZE rounded up
 
 #include <cuda.h>
-
 #include <cuda_runtime.h>
 #include <driver_functions.h>
 
 
-#define STB_IMAGE_IMPLEMENTATION
+// #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image/stb_image.h"
-#define STB_IMAGE_WRITE_IMPLEMENTATION
+// #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image/stb_image_write.h"
 
 ////////////////////////////////////////////////////////////////////////////////////////
@@ -38,109 +38,140 @@
 
 // kernelPfs -- (CUDA device code)
 // TODO: Specific work each cuda block work still needs to be divided.
-__global__ void kernelPfs(int imageWidth, int imageHeight, int channels, unsigned char* img, unsigned char* output) {
+__global__ void kernelPfsBlock(unsigned char* img, int width, int height, int channels, int xblock, int yblock) {
 
-    //TODO: What work needs to be done and how is work split?  
-    
     // Boundary of box we are computing -> Don't know if this is useful but for conceptual understanding
-    int blockMinX = blockIdx.x * blockDim.x;
-    int blockMaxX = blockMinX + blockDim.x;
-    int blockMinY = blockIdx.y * blockDim.y;
-    int blockMaxY = blockMinY + blockDim.y;
+    int blockMinX = threadIdx.x * xblock;
+    int blockMaxX = blockMinX + xblock;
+    int blockMinY = threadIdx.y * yblock;
+    int blockMaxY = blockMinY + yblock;
 
     int threadID =  threadIdx.y * blockDim.x + threadIdx.x;
 
-    unsigned char* pixel = img + (blockIdx.x + imageWidth * blockIdxy) * channels;
-    unsigned char* pixel_right = img + ((blockIdx.x+1) + imageWidth * blockIdx.y) * channels;
-    unsigned char* pixel_bottom_left = img + ((blockIdx.x-1) + imageWidth * (blockIdx.y+1)) * channels;
-    unsigned char* pixel_bottom = img + (blockIdx.x + width * (blockIdx.y+1)) * channels;
-    unsigned char* pixel_bottom_right = img + ((blockIdx.y+1) + imageWidth * (blockIdx.y+1)) * channels;
+    blockMinX = blockMinX == 0 ? 1 : blockMinX;
 
-    int oldR = static_cast<int>(pixel[0]);
-    int oldG = static_cast<int>(pixel[1]);
-    int oldB = static_cast<int>(pixel[1]);
+    //printf("Thread %d : threadIdx.x = %d, threadIdx.y = %d, blockIdx.x = %d, blockIdx.y = %d, \n", threadID, threadIdx.x, threadIdx.y, blockIdx.x, blockIdx.y); 
 
-    int newR = round(FACTOR * oldR / 255.0) * (255/FACTOR);
-    int newG = round(FACTOR * oldG / 255.0) * (255/FACTOR);
-    int newB = round(FACTOR * oldB / 255.0) * (255/FACTOR);
+    printf("Thread %d: threadIdx.x= %d, threadIdx.y= %d, gridDim.x= %d, gridDim.y= %d, blockMinX= %d, blockMaxX= %d, blockMinY= %d, blockMaxY= %d\n", threadID, threadIdx.x, threadIdx.y, gridDim.x, gridDim.y, blockMinX, blockMaxX, blockMinY, blockMaxY);
 
-    pixel[0] = newR;
-    pixel[1] = newG;
-    pixel[2] = newB;
+    for(int y = 0; y < blockMaxY && y < height-1; y++){
+        for(int x = 1; x < blockMaxX && x < width-1; x++){
 
-    int qErrorR = oldR - newR;
-    int qErrorG = oldG - newG;
-    int qErrorB = oldB - newB;
+            unsigned char* pixel = img + (x + width * y) * channels;
+            unsigned char* pixel_right = img + ((x+1) + width * y) * channels;
+            unsigned char* pixel_bottom_left = img + ((x-1) + width * (y+1)) * channels;
+            unsigned char* pixel_bottom = img + (x + width * (y+1)) * channels;
+            unsigned char* pixel_bottom_right = img + ((x+1) + width * (y+1)) * channels;
+            
+            // unsigned char* pixel = img + (blockIdx.x + imageWidth * blockIdx.y) * channels;
+            // unsigned char* pixel_right = img + ((blockIdx.x+1) + imageWidth * blockIdx.y) * channels;
+            // unsigned char* pixel_bottom_left = img + ((blockIdx.x-1) + imageWidth * (blockIdx.y+1)) * channels;
+            // unsigned char* pixel_bottom = img + (blockIdx.x + imageWidth * (blockIdx.y+1)) * channels;
+            // unsigned char* pixel_bottom_right = img + ((blockIdx.x+1) + imageWidth * (blockIdx.y+1)) * channels;
 
-    pixel_right[0] = (uint8_t)(pixel_right[0] + (qErrorR * TOP_RIGHT_ERR_DIFF));
-    pixel_right[1] = (uint8_t)(pixel_right[1] + (qErrorG * TOP_RIGHT_ERR_DIFF));
-    pixel_right[2] = (uint8_t)(pixel_right[2] + (qErrorB * TOP_RIGHT_ERR_DIFF));
+            int oldR = static_cast<int>(pixel[0]);
+            int oldG = static_cast<int>(pixel[1]);
+            int oldB = static_cast<int>(pixel[1]);
 
-    pixel_bottom_left[0] = (uint8_t)(pixel_bottom_left[0] + (qErrorR * BOT_LEFT_ERR_DIFF));
-    pixel_bottom_left[1] = (uint8_t)(pixel_bottom_left[1] + (qErrorG * BOT_LEFT_ERR_DIFF));
-    pixel_bottom_left[2] = (uint8_t)(pixel_bottom_left[2] + (qErrorB * BOT_LEFT_ERR_DIFF));
+            int newR = round(FACTOR * oldR / 255.0) * (255/FACTOR);
+            int newG = round(FACTOR * oldG / 255.0) * (255/FACTOR);
+            int newB = round(FACTOR * oldB / 255.0) * (255/FACTOR);
 
-    pixel_bottom[0] = (uint8_t)(pixel_bottom[0] + (qErrorR * BOT_MID_ERR_DIFF));
-    pixel_bottom[1] = (uint8_t)(pixel_bottom[1] + (qErrorG * BOT_MID_ERR_DIFF));
-    pixel_bottom[2] = (uint8_t)(pixel_bottom[2] + (qErrorB * BOT_MID_ERR_DIFF));
+            pixel[0] = newR;
+            pixel[1] = newG;
+            pixel[2] = newB;
 
-    pixel_bottom_right[0] = (uint8_t)(pixel_bottom_right[0] + (qErrorR * BOT_RIGHT_ERR_DIFF));
-    pixel_bottom_right[1] = (uint8_t)(pixel_bottom_right[1] + (qErrorG * BOT_RIGHT_ERR_DIFF));
-    pixel_bottom_right[2] = (uint8_t)(pixel_bottom_right[2] + (qErrorB * BOT_RIGHT_ERR_DIFF));
-    
-    
-    // idk how this write works - Is it just making the modification? So can we just partially 
-    // write each part.
-    
+            int qErrorR = oldR - newR;
+            int qErrorG = oldG - newG;
+            int qErrorB = oldB - newB;
+
+            pixel_right[0] = (uint8_t)(pixel_right[0] + (qErrorR * TOP_RIGHT_ERR_DIFF));
+            pixel_right[1] = (uint8_t)(pixel_right[1] + (qErrorG * TOP_RIGHT_ERR_DIFF));
+            pixel_right[2] = (uint8_t)(pixel_right[2] + (qErrorB * TOP_RIGHT_ERR_DIFF));
+
+            pixel_bottom_left[0] = (uint8_t)(pixel_bottom_left[0] + (qErrorR * BOT_LEFT_ERR_DIFF));
+            pixel_bottom_left[1] = (uint8_t)(pixel_bottom_left[1] + (qErrorG * BOT_LEFT_ERR_DIFF));
+            pixel_bottom_left[2] = (uint8_t)(pixel_bottom_left[2] + (qErrorB * BOT_LEFT_ERR_DIFF));
+
+            pixel_bottom[0] = (uint8_t)(pixel_bottom[0] + (qErrorR * BOT_MID_ERR_DIFF));
+            pixel_bottom[1] = (uint8_t)(pixel_bottom[1] + (qErrorG * BOT_MID_ERR_DIFF));
+            pixel_bottom[2] = (uint8_t)(pixel_bottom[2] + (qErrorB * BOT_MID_ERR_DIFF));
+
+            pixel_bottom_right[0] = (uint8_t)(pixel_bottom_right[0] + (qErrorR * BOT_RIGHT_ERR_DIFF));
+            pixel_bottom_right[1] = (uint8_t)(pixel_bottom_right[1] + (qErrorG * BOT_RIGHT_ERR_DIFF));
+            pixel_bottom_right[2] = (uint8_t)(pixel_bottom_right[2] + (qErrorB * BOT_RIGHT_ERR_DIFF));
+        }
+    }
 
     return;
-
 }
 
 void pfsCuda(int width, int height, int channels, unsigned char *img) {
 
-    // Define dimensions for work assignment
-    dim3 blockDim(BLOCKDIMCOMP, BLOCKDIMCOMP);
-    dim3 gridDim((width + blockDim.x - 1) / blockDim.x, (height + blockDim.y - 1) / blockDim.y);
+    printf(" In CUDA code\n");
 
-    unsigned char *cudaDeviceImageData;
+    stbi_write_png("../images/before-parallel.png", width, height, channels, img, width * channels);
+
+    // Define dimensions for work assignment
+    // dim3 blockDim(BLOCKDIMCOMP, BLOCKDIMCOMP);
+    // dim3 gridDim((width + blockDim.x - 1) / blockDim.x, (height + blockDim.y - 1) / blockDim.y);
+
+    dim3 blockDim(BLOCKDIMCOMP, BLOCKDIMCOMP);
+    dim3 gridDim(1, 1);
+
+    int xblock = (width + blockDim.x - 1) / blockDim.x;
+    int yblock = (height + blockDim.y - 1) / blockDim.y;
+    
+
+    int bytes = sizeof(uint8_t) * width * height * channels;
+
+    unsigned char *device_img;
+    unsigned char *result = new unsigned char[width * height * channels];
+
+    printf(" Set arrays\n");
 
     // Allocate device memory buffers on the GPU using cudaMalloc
     //TODO: Slightly confused what needs to be malloced on device - not sure if this is correct
-    cudaMalloc(&cudaDeviceImageData, sizeof(uint8_t) * width * height * channels);
-
+    cudaMalloc(&device_img, bytes);
 
     // TODO copy input arrays to the GPU using cudaMemcpy
-    // cudaMemcpy( cudaMemcpyHostToDevice);
-    unsigned char *outputImageData;
+    cudaMemcpy(device_img, img, bytes, cudaMemcpyHostToDevice);
+
+    printf(" Arrays Copied\n");
+
+    double startKernelTime = CycleTimer::currentSeconds();
 
     // run kernel
-    kernelPfs<<<gridDim, blockDim>>>(imageHeight, imageWidth, channels, img, outputImageData);
-
-    // TODO: Uncomment and check for correctness that image still renders
-    // kernelPfs<<<1, 1>>>();
+    kernelPfsBlock<<<gridDim, blockDim>>>(device_img, width, height, channels, xblock, yblock);
+    //kernelPfsBlock<<<1, 1>>>(device_img, width, height, channels);
 
     cudaDeviceSynchronize();
+    double endKernelTime = CycleTimer::currentSeconds();
+
+    printf(" CUDA finished\n");
+    double kernelDuration = endKernelTime - startKernelTime;
+    printf(" Kernel Time: %f\n", kernelDuration);
    
-    unsigned char *outputImageData;
     // copy result from GPU using cudaMemcpy: Idea is to copy computed image to local
-    cudaMemcpy(outputImageData, cudaDeviceImageData, sizeof(uint8_t), cudaMemcpyDeviceToHost);
+    cudaMemcpy(result, device_img, bytes, cudaMemcpyDeviceToHost);
+
+    printf(" Copied from CUDA\n");
 
     cudaError_t errCode = cudaPeekAtLastError();
     if (errCode != cudaSuccess) {
         fprintf(stderr, "WARNING: A CUDA error occured: code=%d, %s\n", errCode, cudaGetErrorString(errCode));
     }
 
-    cout << "Generated Color Dithered image" << endl; 
-    stbi_write_png("../images/dither.png", imageWidth, imageHeight, channels, outputImageData, imageWidth * channels);
-    cout << "Wrote Color Dithered image" << endl; 
+    printf(" Generated Color Dithered image\n"); 
+    stbi_write_png("../images/dither-parallel.png", width, height, channels, result, width * channels);
+    printf(" Wrote Color Dithered image\n"); 
 
     // free memory buffers on the GPU
-    cudaFree(cudaDeviceImageData);
+    cudaFree(device_img);
 
 }
 
-int main () {
+int cudaMain() {
 
     //Load image 
     int width, height, channels;
