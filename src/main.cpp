ac5a6a4427e622d8dd90c6b65f3bb30798cb4c0a
Andy Chen <andyc2@andrew.cmu.edu>
2021-05-03 16:24:16 -0400

    Improved speedup on OMP by chunking image

--- src/main.cpp
+++ src/main.cpp
@@ -107,51 +107,93 @@ void gray(unsigned char *img, int width, int height, int channels){
     free(gray_img);
 }
 
-void omp_parallel_dither(unsigned char *img, int width, int height, int channels){
 
-    int totalPixels = height * width;
-    #pragma omp parallel for num_threads(NCORES)
-    for(int y = 0; y < height-1; y++){
+void dither(unsigned char *img, int newStart, int width, int height, int channels) {
+
+    for(int y = newStart; y < newStart + height - 1; y++){
         for(int x = 1; x < width-1; x++){
-            unsigned char* pixel = img + (x + width * y) * channels;
-            unsigned char* pixel_right = img + ((x+1) + width * y) * channels;
-            unsigned char* pixel_bottom_left = img + ((x-1) + width * (y+1)) * channels;
-            unsigned char* pixel_bottom = img + (x + width * (y+1)) * channels;
-            unsigned char* pixel_bottom_right = img + ((x+1) + width * (y+1)) * channels;
+                unsigned char* pixel = img + (x + width * y) * channels;
+                unsigned char* pixel_right = img + ((x+1) + width * y) * channels;
+                unsigned char* pixel_bottom_left = img + ((x-1) + width * (y+1)) * channels;
+                unsigned char* pixel_bottom = img + (x + width * (y+1)) * channels;
+                unsigned char* pixel_bottom_right = img + ((x+1) + width * (y+1)) * channels;
 
-            int oldR = static_cast<int>(pixel[0]);
-            int oldG = static_cast<int>(pixel[1]);
-            int oldB = static_cast<int>(pixel[1]);
+                int oldR = static_cast<int>(pixel[0]);
+                int oldG = static_cast<int>(pixel[1]);
+                int oldB = static_cast<int>(pixel[1]);
 
-            int newR = round(FACTOR * oldR / 255.0) * (255/FACTOR);
-            int newG = round(FACTOR * oldG / 255.0) * (255/FACTOR);
-            int newB = round(FACTOR * oldB / 255.0) * (255/FACTOR);
+                int newR = round(FACTOR * oldR / 255.0) * (255/FACTOR);
+                int newG = round(FACTOR * oldG / 255.0) * (255/FACTOR);
+                int newB = round(FACTOR * oldB / 255.0) * (255/FACTOR);
 
-            int qErrorR = oldR - newR;
-            int qErrorG = oldG - newG;
-            int qErrorB = oldB - newB;
+                int qErrorR = oldR - newR;
+                int qErrorG = oldG - newG;
+                int qErrorB = oldB - newB;
 
-            pixel[0] = newR;
-            pixel[1] = newG;
-            pixel[2] = newB;
+                pixel[0] = newR;
+                pixel[1] = newG;
+                pixel[2] = newB;
 
-            pixel_right[0] = (uint8_t)(pixel_right[0] + (qErrorR * (7.0 / 16.0)));
-            pixel_right[1] = (uint8_t)(pixel_right[1] + (qErrorG * (7.0 / 16.0)));
-            pixel_right[2] = (uint8_t)(pixel_right[2] + (qErrorB * (7.0 / 16.0)));
+                #pragma omp atomic
+                pixel_right[0] += (qErrorR * (7.0 / 16.0));
+                #pragma omp atomic
+                pixel_right[1] += (qErrorG * (7.0 / 16.0));
+                #pragma omp atomic
+                pixel_right[2] += (qErrorB * (7.0 / 16.0));
 
-            pixel_bottom_left[0] = (uint8_t)(pixel_bottom_left[0] + (qErrorR * (3.0 / 16.0)));
-            pixel_bottom_left[1] = (uint8_t)(pixel_bottom_left[1] + (qErrorG * (3.0 / 16.0)));
-            pixel_bottom_left[2] = (uint8_t)(pixel_bottom_left[2] + (qErrorB * (3.0 / 16.0)));
+                // pixel_right[0] = (uint8_t)(pixel_right[0] + (qErrorR * (7.0 / 16.0)));
+                // pixel_right[1] = (uint8_t)(pixel_right[1] + (qErrorG * (7.0 / 16.0)));
+                // pixel_right[2] = (uint8_t)(pixel_right[2] + (qErrorB * (7.0 / 16.0)));
 
-            pixel_bottom[0] = (uint8_t)(pixel_bottom[0] + (qErrorR * (5.0 / 16.0)));
-            pixel_bottom[1] = (uint8_t)(pixel_bottom[1] + (qErrorG * (5.0 / 16.0)));
-            pixel_bottom[2] = (uint8_t)(pixel_bottom[2] + (qErrorB * (5.0 / 16.0)));
+                #pragma omp atomic
+                pixel_bottom_left[0] += (qErrorR * (3.0 / 16.0));
+                #pragma omp atomic
+                pixel_bottom_left[1] += (qErrorG * (3.0 / 16.0));
+                #pragma omp atomic
+                pixel_bottom_left[2] += (qErrorB * (3.0 / 16.0));
+                // pixel_bottom_left[0] = (uint8_t)(pixel_bottom_left[0] + (qErrorR * (3.0 / 16.0)));
+                // pixel_bottom_left[1] = (uint8_t)(pixel_bottom_left[1] + (qErrorG * (3.0 / 16.0)));
+                // pixel_bottom_left[2] = (uint8_t)(pixel_bottom_left[2] + (qErrorB * (3.0 / 16.0)));
 
-            pixel_bottom_right[0] = (uint8_t)(pixel_bottom_right[0] + (qErrorR * (1.0 / 16.0)));
-            pixel_bottom_right[1] = (uint8_t)(pixel_bottom_right[1] + (qErrorG * (1.0 / 16.0)));
-            pixel_bottom_right[2] = (uint8_t)(pixel_bottom_right[2] + (qErrorB * (1.0 / 16.0))); 
+                #pragma omp atomic
+                pixel_bottom[0] += (qErrorR * (5.0 / 16.0));
+                #pragma omp atomic
+                pixel_bottom[1] += (qErrorG * (5.0 / 16.0));
+                #pragma omp atomic
+                pixel_bottom[2] += (qErrorB * (5.0 / 16.0));
+                // pixel_bottom[0] = (uint8_t)(pixel_bottom[0] + (qErrorR * (5.0 / 16.0)));
+                // pixel_bottom[1] = (uint8_t)(pixel_bottom[1] + (qErrorG * (5.0 / 16.0)));
+                // pixel_bottom[2] = (uint8_t)(pixel_bottom[2] + (qErrorB * (5.0 / 16.0)));
+
+                #pragma omp atomic
+                pixel_bottom_right[0] += (qErrorR * (1.0 / 16.0));
+                #pragma omp atomic
+                pixel_bottom_right[1] += (qErrorG * (1.0 / 16.0));
+                #pragma omp atomic
+                pixel_bottom_right[2] += (qErrorB * (1.0 / 16.0));
+                // pixel_bottom_right[0] = (uint8_t)(pixel_bottom_right[0] + (qErrorR * (1.0 / 16.0)));
+                // pixel_bottom_right[1] = (uint8_t)(pixel_bottom_right[1] + (qErrorG * (1.0 / 16.0)));
+                // pixel_bottom_right[2] = (uint8_t)(pixel_bottom_right[2] + (qErrorB * (1.0 / 16.0))); 
         }
     }
+}
+
+
+// Blocking Strategy Adapted from : https://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.734.9930&rep=rep1&type=pdf 
+void omp_parallel_dither(unsigned char *img, int width, int height, int channels){
+
+    // int totalPixels = height * width;
+
+
+    #pragma omp parallel for num_threads(NCORES)
+    for (int i = 0; i < NCORES; i++) {
+        int dividedHeight = height / omp_get_num_threads();
+        int newStart = dividedHeight * i; 
+
+        // cout << "New Start : " << newStart <<  ", Thread: " << omp_get_thread_num() << endl;
+        dither(img, newStart, width, dividedHeight, channels);
+    }
+    
     
     cout << "Generated OMP Color Dithered image" << endl; 
 
@@ -170,9 +212,9 @@ int main(void) {
     
 
     int width, height, channels;
-    unsigned char *img = stbi_load("../images/beach.png", &width, &height, &channels, 0);
-    unsigned char *og_img = stbi_load("../images/beach.png", &width, &height, &channels, 0);
-    unsigned char *omp_img = stbi_load("../images/beach.png", &width, &height, &channels, 0);
+    unsigned char *img = stbi_load("../images/roadster.png", &width, &height, &channels, 0);
+    unsigned char *og_img = stbi_load("../images/roadster.png", &width, &height, &channels, 0);
+    unsigned char *omp_img = stbi_load("../images/roadster.png", &width, &height, &channels, 0);
     if(img == NULL) {
         printf("Error in loading the image\n");
         exit(1);
@@ -196,8 +238,8 @@ int main(void) {
     
     cout << "Starting Parallel Version..." << endl;
 
-    printCudaInfo();
-    pfsCuda(width, height, channels, og_img);
+    //printCudaInfo();
+    //pfsCuda(width, height, channels, og_img);
 
 
     stbi_image_free(img);
